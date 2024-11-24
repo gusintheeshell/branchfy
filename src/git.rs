80678cee2cd18dee97eeddf566aa2541f045d424
Gustavo Barbosa <gusbdev@gmail.com>
2024-11-24 10:38:22 -0300

    refactor: remove unused configuration and player modules

--- src/git.rs
+++
@@ -1,7 +0,0 @@
-use git2::Repository;
-
-pub fn get_current_branch() -> Result<String, git2::Error> {
-    let repo = Repository::open(".")?;
-    let head = repo.head()?;
-    Ok(head.shorthand().unwrap_or("").to_string())
-}

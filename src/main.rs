9243d09a1ac9937e70f49413b24802c5e17d3b70
Gustavo Barbosa <gusbdev@gmail.com>
2024-11-24 00:57:13 -0300

    feat: enhance main function to load config and play playlist based on current git branch

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
-fn main() {
-    println!("Hello, world!");
+mod config;
+mod git;
+mod player;
+
+use crate::config::Config;
+use crate::git::get_current_branch;
+use crate::player::Player;
+use std::process::exit;
+
+#[tokio::main]
+async fn main() {
+    let config = Config::load_or_create().unwrap_or_else(|err| {
+        eprintln!("Erro ao carregar as configurações: {}", err);
+        exit(1);
+    });
+
+    let branch = get_current_branch().unwrap_or_else(|err| {
+        eprintln!("Erro ao detectar branch: {}", err);
+        exit(1);
+    });
+
+    if let Some(prefix) = config.get_playlist_for_branch(&branch) {
+        println!("Tocando playlist para o prefixo: {}", prefix);
+        Player::play_playlist(&config, prefix).await;
+    } else {
+        println!("Nenhuma playlist associada a este prefixo de branch.");
+    }
+
+    ctrlc::set_handler(move || {
+        println!("Encerrando player...");
+        exit(0);
+    })
+    .expect("Erro ao definir handler de Ctrl+C");
 }

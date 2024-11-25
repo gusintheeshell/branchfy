4932d824e6a5a864bab36fe88070b00291eaeab2
Gustavo Barbosa <gusbdev@gmail.com>
2024-11-25 18:35:09 -0300

    feat: add progress display for currently playing track in Player

--- src/player/mod.rs
+++ src/player/mod.rs
@@ -1,15 +1,62 @@
 use crate::config::Config;
+use indicatif::{ProgressBar, ProgressStyle};
 use rspotify::model::{PlayableItem, PlaylistId, Token};
 use rspotify::prelude::*;
 use rspotify::{scopes, AuthCodeSpotify, Credentials, OAuth};
 use std::fs;
 use std::io::{self, BufRead};
+use std::time::Duration;
 
 const TOKEN_FILE: &str = "spotify_token.json";
 
 pub struct Player;
 
 impl Player {
+    fn format_duration(ms: u32) -> String {
+        let total_seconds = ms / 1000;
+        let minutes = total_seconds / 60;
+        let seconds = total_seconds % 60;
+        format!("{:02}:{:02}", minutes, seconds)
+    }
+
+    async fn display_progress(
+        spotify: &AuthCodeSpotify,
+        track_name: String,
+        progress_ms: u64,
+        duration_ms: u32,
+    ) {
+        let pb = ProgressBar::new(duration_ms as u64);
+        pb.set_style(
+            ProgressStyle::default_bar()
+                .template("{spinner:.green} {msg}\n[{elapsed_precise}] [{bar:40.cyan/blue}] [{pos}/{len}]")
+                .unwrap()
+                .progress_chars("=>-"),
+        );
+
+        pb.set_message(format!("Playing: {}", track_name));
+        pb.set_position(progress_ms);
+
+        loop {
+            if let Ok(Some(playback)) = spotify.current_playback(None, None::<&[_]>).await {
+                if let Some(progress_ms) = playback.progress {
+                    // Changed from progress to progress_ms
+                    pb.set_position(progress_ms.num_milliseconds() as u64);
+
+                    // Format progress/total time
+                    let current = Self::format_duration(progress_ms.num_milliseconds() as u32);
+                    let total = Self::format_duration(duration_ms);
+                    pb.set_message(format!("Playing: {} ({}/{})", track_name, current, total));
+
+                    if progress_ms >= chrono::Duration::milliseconds(duration_ms as i64) {
+                        pb.finish_with_message(format!("Finished: {} ({})", track_name, total));
+                        break;
+                    }
+                }
+            }
+            tokio::time::sleep(Duration::from_secs(1)).await;
+        }
+    }
+
     pub async fn play_playlist(_config: &Config, playlist_id: &str) {
         async fn authenticate_spotify(
             spotify: &AuthCodeSpotify,
@@ -32,6 +79,33 @@ impl Player {
                 fs::write(TOKEN_FILE, serde_json::to_string(&token)?)?;
             } else {
                 return Err("Failed to get access token".into());
+            }
+
+            if let Some(current_playback) =
+                spotify.current_playback(None, None::<&[_]>).await.unwrap()
+            {
+                if let Some(item) = current_playback.item {
+                    match item {
+                        PlayableItem::Track(track) => {
+                            if let (Some(progress), Some(duration)) =
+                                (current_playback.progress, Some(track.duration))
+                            {
+                                let progress_ms = progress.num_milliseconds() as u32;
+                                let duration_ms = duration.num_milliseconds() as u32;
+                                Player::display_progress(
+                                    &spotify,
+                                    track.name.clone(),
+                                    progress_ms.into(),
+                                    duration_ms,
+                                )
+                                .await;
+                            }
+                        }
+                        PlayableItem::Episode(episode) => {
+                            println!("Playing episode: {}", episode.name)
+                        }
+                    }
+                }
             }
 
             Ok(())
